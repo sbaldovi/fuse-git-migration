bf5ae3e5bf15fb1286c7a2dda423562e389e109d
Sergio Baldoví <serbalgi@gmail.com>
2016-06-19 10:25:10 +0200

    Add script fuse2git.sh

---
+++ fuse2git.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Convert to Git
+make || exit 1
+
+# Tag svn conversion at r2858: 2007-05-19T16:19:04Z!philip-fuse@shadowmagic.org.uk
+cd fuse-emulator-git
+git tag -a conversion-to-svn \
+    $(git log --grep '^Legacy-ID: 2858$' --abbrev-commit | head -n 1 | cut -d ' ' -f 2) \
+    -m 'Marks the spot at which this repository was converted from CVS to Subversion.'
+cd ..
+
+# Run a garbage-collect on the generated git repository
+make gc
