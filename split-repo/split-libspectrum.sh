6cb3806020fe7cca1f4bb0fc203a1d94ff31a6bf
Sergio Baldoví <serbalgi@gmail.com>
2016-08-03 22:55:52 +0200

    fuse/libspectrum split scripts can reparent merge commits.
    Reparenting existing trunk->branch sync points allow easier final merge.

--- split-repo/split-libspectrum.sh
+++ split-repo/split-libspectrum.sh
@@ -6,17 +6,48 @@
 # Requires modified git-subtree.sh
 
 SOURCE_REPO=../fuse-emulator-git/
-TMP_REPO=tmp-libspectrum
+TMP_SPLIT_REPO=tmp-libspectrum-split
+TMP_PUSH_REPO=tmp-libspectrum-push
+TMP_REPARENT_REPO=tmp-libspectrum-reparent
 LIBSPECTRUM_REPO=libspectrum
 
+# Split prefix and push to branch
 split_branch() {
   echo
   echo " * Branch $1"
 
-  git checkout -B subtree_$1 origin/$1
-  ../../bin/git-subtree.sh split --prefix=libspectrum -b subtree_libspectrum_$1
-  git push ../$LIBSPECTRUM_REPO subtree_libspectrum_$1:$1
+  git checkout -B $1 origin/$1
+  ../../bin/git-subtree.sh split --prefix=libspectrum -b subtree_$1
+  git push ../$TMP_PUSH_REPO subtree_$1:$1
 }
+
+# Change parents based on svn revisions
+graft_merge()
+{
+  merge_hash=$(git log --all --grep '^Legacy-ID: '$1'$' | head -n 1 | cut -d ' ' -f 2)
+  parent1_hash=$(git log --all --grep '^Legacy-ID: '$2'$' | head -n 1 | cut -d ' ' -f 2)
+  parent2_hash=$(git log --all --grep '^Legacy-ID: '$3'$' | head -n 1 | cut -d ' ' -f 2)
+  echo "$merge_hash $parent1_hash $parent2_hash" >> .git/info/grafts
+  git update-server-info
+}
+
+# Make grafts permanent
+filter_branch()
+{
+  branch=$(git rev-parse --abbrev-ref HEAD)
+  merge_base=$(git merge-base $branch master)
+  echo "git filter-branch -- $merge_base..HEAD"
+  git filter-branch -- $merge_base..HEAD
+
+  rm .git/info/grafts
+  git update-server-info
+  git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
+}
+
+if test ! -d "$SOURCE_REPO"; then
+  echo "error: missing $SOURCE_REPO"
+  exit 1
+fi
 
 if test ! -x "../bin/git-subtree.sh"; then
   echo "error: missing ../bin/git-subtree.sh"
@@ -28,19 +59,52 @@ rm -rf $LIBSPECTRUM_REPO
 git init --bare $LIBSPECTRUM_REPO
 
 # Clone to temporary repository
-rm -rf $TMP_REPO
-git clone $SOURCE_REPO --no-hardlinks -- $TMP_REPO
+rm -rf $TMP_SPLIT_REPO
+git clone $SOURCE_REPO --no-hardlinks -- $TMP_SPLIT_REPO
+
+# Create temporary repository
+rm -rf $TMP_PUSH_REPO
+git init --bare $TMP_PUSH_REPO
 
 # Split and push active branches
-cd $TMP_REPO
+cd $TMP_SPLIT_REPO
 split_branch master
 split_branch 2014-09-07-ulaplus
 #split_branch 2014-12-28-sdl2
 #split_branch 2016-04-25-didaktik
-split_branch patches-356-threadsafe-libspectrum
 split_branch feature-53-zip-support
+#split_branch feature-100-remote-debugger
+split_branch patches-356-threadsafe-libspectrum
+#split_branch patches-377-more-startup-manager
 cd ..
-rm -rf $TMP_REPO
+rm -rf $TMP_SPLIT_REPO
+
+# Clone to temporary repository
+rm -rf $TMP_REPARENT_REPO
+git clone $TMP_PUSH_REPO --no-hardlinks -- $TMP_REPARENT_REPO
+
+# Make branches local
+cd $TMP_REPARENT_REPO
+git checkout 2014-09-07-ulaplus
+#git checkout 2014-12-28-sdl2
+#git checkout 2016-04-25-didaktik
+git checkout feature-53-zip-support
+#git checkout feature-100-remote-debugger
+git checkout patches-356-threadsafe-libspectrum
+#git checkout patches-377-more-startup-manager
+git remote remove origin
+
+# Reparent sync merges (trunk->branch)
+git checkout 2014-09-07-ulaplus
+graft_merge 5341 5323 5316
+graft_merge 5609 5345 5603
+filter_branch
+
+# Push to final repo
+git push --all ../$LIBSPECTRUM_REPO
+cd ..
+rm -rf $TMP_REPARENT_REPO
+rm -rf $TMP_PUSH_REPO
 
 # Prune reflogs
 cd $LIBSPECTRUM_REPO
