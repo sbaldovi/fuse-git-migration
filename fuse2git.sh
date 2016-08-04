640920f8b8a5e54a20a33bbecb5a23693c787e42
Sergio Baldoví <serbalgi@gmail.com>
2016-08-05 00:01:08 +0200

    Main script split the big repo in individual pieces

--- fuse2git.sh
+++ fuse2git.sh
@@ -13,3 +13,11 @@ make 2>&1 | tee fuse-emulator.log || exit 1
 
 # Run a garbage-collect on the generated git repository
 make gc
+
+# Split big git respository into individual respositories
+cd split-repo
+./split-non-core.sh || exit 1
+./split-fuse-utils.sh || exit 1
+./split-libspectrum.sh || exit 1
+./split-fuse.sh || exit 1
+cd ..
